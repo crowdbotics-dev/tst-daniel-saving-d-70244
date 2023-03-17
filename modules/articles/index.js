8a3754d8dcee85369a0a20a3cba49e90d995a36c
Crowdbotics <engineering@crowdbotics.com>
2023-03-17 15:27:32 +0000

    add component Articles

--- modules/articles/index.js
+++ modules/articles/index.js
@@ -1,50 +1,63 @@
 import React, { useEffect } from "react";
-import { Text, FlatList, View, TouchableOpacity, ImageBackground, SafeAreaView } from "react-native";
+import {
+  Text,
+  FlatList,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  SafeAreaView
+} from "react-native";
 import { styles } from "./styles";
 import { slice, articleList } from "./store";
 import { useSelector, useDispatch } from "react-redux";
 import { createStackNavigator } from "@react-navigation/stack";
 import Article from "./article";
 
-const ArticlesList = ({
-  route,
-  navigation
-}) => {
+const ArticlesList = ({ route, navigation }) => {
   const detail = route.params?.detail || "Article";
-  const articles = useSelector(state => Object.entries(state.Articles.articles).map(([, entry]) => entry));
+  const articles = useSelector((state) =>
+    Object.entries(state.Articles.articles).map(([, entry]) => entry)
+  );
   const dispatch = useDispatch();
+
   useEffect(async () => {
-    dispatch(articleList()).catch(e => console.log(e.message));
+    dispatch(articleList()).catch((e) => console.log(e.message));
   }, [detail]);
 
-  const renderItem = ({
-    item
-  }) => <TouchableOpacity onPress={() => {
-    navigation.navigate(detail, {
-      id: item.id
-    });
-  }}>
-      <ImageBackground source={{
-      uri: item.image
-    }} style={styles.image}>
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate(detail, { id: item.id });
+      }}
+    >
+      <ImageBackground source={{ uri: item.image }} style={styles.image}>
         <View style={styles.card}>
           <Text style={styles.text}>{item.title}</Text>
           <Text style={styles.author}>{item.author}</Text>
         </View>
       </ImageBackground>
-    </TouchableOpacity>;
+    </TouchableOpacity>
+  );
 
-  return <SafeAreaView>
-      <FlatList data={articles} renderItem={renderItem} keyExtractor={item => `${item.id}`} />
-    </SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={articles}
+        renderItem={renderItem}
+        keyExtractor={(item) => `${item.id}`}
+      />
+    </SafeAreaView>
+  );
 };
 
 const Stack = createStackNavigator();
 
-const ArticlesNavigator = () => <Stack.Navigator headerMode="none" initialRouteName="Articles">
+const ArticlesNavigator = () => (
+  <Stack.Navigator headerMode="none" initialRouteName="Articles">
     <Stack.Screen name="Articles" component={ArticlesList} />
     <Stack.Screen name="Article" component={Article} />
-  </Stack.Navigator>;
+  </Stack.Navigator>
+);
 
 export default {
   title: "Articles",
