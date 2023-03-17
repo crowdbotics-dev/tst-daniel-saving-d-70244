0519acbda3efd8a1c4ba8e81e274d99fd1d04c9b
Daniel Sousa <daniel.s@crowdbotics.com>
2023-03-17 15:31:25 +0000

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,4 @@
+import { View } from "react-native";
 import { Text } from "react-native";
 import { TextInput } from "react-native";
 import React from "react";
@@ -10,7 +11,7 @@ const Untitled1 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><Text style={styles.uxjTSOXq}>Title</Text><TextInput style={styles.OJhIZYkw}></TextInput><Text style={styles.SPMGQgmK}>Body</Text><TextInput style={styles.cBJjmzTC}></TextInput><Text style={styles.pbUIzBKw}>Author</Text><TextInput style={styles.rNTEuKVc}></TextInput><Text style={styles.FbCwlETk}>Image</Text><TextInput style={styles.pJSFSYvC}></TextInput></ScrollView>
+    }}><Text style={styles.uxjTSOXq}>Title</Text><TextInput style={styles.OJhIZYkw}></TextInput><Text style={styles.SPMGQgmK}>Body</Text><TextInput style={styles.cBJjmzTC}></TextInput><Text style={styles.pbUIzBKw}>Author</Text><TextInput style={styles.rNTEuKVc}></TextInput><Text style={styles.FbCwlETk}>Image</Text><TextInput style={styles.pJSFSYvC}></TextInput><View style={styles.ywIjYjOK}><Text style={styles.iHYojExH}>Save</Text></View></ScrollView>
     </SafeAreaView>;
 };
 
@@ -76,6 +77,24 @@ const styles = StyleSheet.create({
     borderColor: "#cccccc",
     width: 334,
     height: 30
+  },
+  ywIjYjOK: {
+    height: 48,
+    width: 333,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    left: 12,
+    top: 246
+  },
+  iHYojExH: {
+    width: 332,
+    height: 48,
+    lineHeight: 44,
+    fontSize: 14,
+    borderRadius: 0,
+    textAlign: "center"
   }
 });
 export default Untitled1;
