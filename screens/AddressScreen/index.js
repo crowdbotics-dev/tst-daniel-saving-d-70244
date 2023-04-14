78d3fbf5c0702fcc111883c56398f6b856c2d848
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-04-14 16:50:03 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/AddressScreen/index.js
+++ screens/AddressScreen/index.js
@@ -3,67 +3,66 @@ import { Text, StyleSheet, View, Image, ScrollView, TouchableHighlight, Pressabl
 import { useDispatch, useSelector } from "react-redux";
 import { deleteUserAddress, getUserAddress } from "../../store";
 
-const AddressScreen = ({ navigation, route }) => {
+const AddressScreen = ({
+  navigation,
+  route
+}) => {
   const dispatch = useDispatch();
   const [userAddresses, setUserAddresses] = useState([]);
-  const [currentAddress, setCurrentAddress] = useState({})
+  const [currentAddress, setCurrentAddress] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [searchedAddress, setSearchedAddress] = useState([]);
   const userAddress = useSelector(state => state?.ecommerce?.userAddress);
-
   useEffect(() => {
-    setIsLoading(true)
-    const resultLength = userAddress.length
+    setIsLoading(true);
+    const resultLength = userAddress.length;
     setUserAddresses(userAddress);
-    setSearchedAddress(userAddress)
-    setCurrentAddress(userAddress[resultLength - 1])
-    setIsLoading(false)
-  }, [userAddress])
+    setSearchedAddress(userAddress);
+    setCurrentAddress(userAddress[resultLength - 1]);
+    setIsLoading(false);
+  }, [userAddress]);
 
-
-  const updateCurrentAddress = (address) => {
+  const updateCurrentAddress = address => {
     setCurrentAddress(address);
-  }
+  };
 
-  const handleDeleteAddress = async (id) => {
-    setIsLoading(true)
-    await dispatch(deleteUserAddress(id)).then(async (res) => {
-      await dispatch(getUserAddress()).then((response) => {
-        setIsLoading(false)
-      }).catch((error) => {
+  const handleDeleteAddress = async id => {
+    setIsLoading(true);
+    await dispatch(deleteUserAddress(id)).then(async res => {
+      await dispatch(getUserAddress()).then(response => {
         setIsLoading(false);
-        console.log("Error: ", error)
-      })
-    }).catch((err) => {
+      }).catch(error => {
+        setIsLoading(false);
+        console.log("Error: ", error);
+      });
+    }).catch(err => {
       console.log("Error: ", err);
       setIsLoading(false);
-    })
-  }
+    });
+  };
 
-
-  const handleSearch = async (text) => {
+  const handleSearch = async text => {
     if (!text) {
-      setUserAddresses(searchedAddress)
+      setUserAddresses(searchedAddress);
     } else {
       const filterList = userAddresses.filter(element => element.line1.toLowerCase().includes(text.toLowerCase()));
       setUserAddresses(filterList);
     }
-  }
+  };
 
   const getUserAddresses = () => {
-    dispatch(getUserAddress()).then((res) => { }).catch((err) => console.log("Error: ", err))
-  }
+    dispatch(getUserAddress()).then(res => {}).catch(err => console.log("Error: ", err));
+  };
+
   useEffect(() => {
     getUserAddresses();
-  }, [])
-
-  return (
-    <ScrollView style={[styles.container]} showsVerticalScrollIndicator={false}>
+  }, []);
+  return <ScrollView style={[styles.container]} showsVerticalScrollIndicator={false}>
       {isLoading && <Loader></Loader>}
       <View style={styles.searchContainer}>
         <Text style={styles.headText}>Current address</Text>
-        <View style={[styles.inputText, { paddingVertical: 12, paddingLeft: 10 }]}>
-          <View style={{ flex: 1 }}>
+        <View style={[styles.inputText, styles.jmoZTrWu]}>
+          <View style={styles.kuKNRXiv}>
             <Text numberOfLines={1}>{currentAddress?.line1}</Text>
           </View>
           <Image source={require("./assets/map.png")} style={styles.mr10} />
@@ -75,7 +74,7 @@ const AddressScreen = ({ navigation, route }) => {
       <View style={styles.searchContainer}>
         <Text style={[styles.headText]}>Search</Text>
         <View style={styles.inputText}>
-          <View style={{ flex: 1 }}>
+          <View style={styles.xgmPqukr}>
             <Input placeholder='Search' onChangeText={handleSearch} />
           </View>
           <Image source={require("./assets/search.png")} style={styles.mr10} />
@@ -83,29 +82,26 @@ const AddressScreen = ({ navigation, route }) => {
       </View>
 
       <Text style={styles.subheading}>My locations</Text>
-      {
-        userAddresses && userAddresses.map((address, index) =>
-          <View style={styles.searchContainer} key={index}>
-            <Text style={{}}></Text>
+      {userAddresses && userAddresses.map((address, index) => <View style={styles.searchContainer} key={index}>
+            <Text style={styles.lHKXBkbq}></Text>
             <Pressable style={styles.addressContainer} onPress={() => updateCurrentAddress(address)}>
-              <View style={{ flex: 1 }}>
+              <View style={styles.yRwCRzPg}>
                 <Text numberOfLines={1}>{address?.line1}</Text>
               </View>
               <Pressable onPress={() => handleDeleteAddress(address?.id)}>
                 <Text style={styles.addressDelete}>Delete</Text>
               </Pressable>
             </Pressable>
-          </View>
-        )
-      }
+          </View>)}
 
 
       <Pressable onPress={() => navigation.navigate("mapScreen")}>
         <Text style={styles.addAddress}>+ Add new location</Text>
       </Pressable>
-      <Button buttonText={"Update"} onPress={() => navigation.navigate("checkoutScreen", { currentAddress })} />
-    </ScrollView>
-  );
+      <Button buttonText={"Update"} onPress={() => navigation.navigate("checkoutScreen", {
+      currentAddress
+    })} />
+    </ScrollView>;
 };
 
 const styles = StyleSheet.create({
@@ -147,38 +143,61 @@ const styles = StyleSheet.create({
     height: 124,
     width: 370,
     borderRadius: 15,
-    overflow: "hidden",
+    overflow: "hidden"
   },
   mapContainer: {
-    marginTop: 20, marginBottom: 10, height: 124,
+    marginTop: 20,
+    marginBottom: 10,
+    height: 124,
     width: 370,
     borderRadius: 15
   },
-  subheading: { fontSize: 16, color: "#1E2022", fontWeight: "bold", marginTop: 30, marginBottom: 5, paddingLeft: 10, paddingBottom: 10, borderBottomColor: "rgba(217, 217, 217, 0.3);", borderBottomWidth: 1 },
-  addressDelete: { fontSize: 14, color: "#EA4335", fontWeight: "bold", marginRight: 10, },
-  addAddress: { fontSize: 14, color: "#12D790", fontWeight: "bold", marginLeft: 10, marginVertical: 20 },
-})
-
-
+  subheading: {
+    fontSize: 16,
+    color: "#1E2022",
+    fontWeight: "bold",
+    marginTop: 30,
+    marginBottom: 5,
+    paddingLeft: 10,
+    paddingBottom: 10,
+    borderBottomColor: "rgba(217, 217, 217, 0.3);",
+    borderBottomWidth: 1
+  },
+  addressDelete: {
+    fontSize: 14,
+    color: "#EA4335",
+    fontWeight: "bold",
+    marginRight: 10
+  },
+  addAddress: {
+    fontSize: 14,
+    color: "#12D790",
+    fontWeight: "bold",
+    marginLeft: 10,
+    marginVertical: 20
+  },
+  jmoZTrWu: {
+    paddingVertical: 12,
+    paddingLeft: 10
+  },
+  kuKNRXiv: {
+    flex: 1
+  },
+  xgmPqukr: {
+    flex: 1
+  },
+  lHKXBkbq: {},
+  yRwCRzPg: {
+    flex: 1
+  }
+});
 export default AddressScreen;
-
-
-export const Input = (props) => {
-  return (
-    <View>
-      <TextInput
-        style={textStyles.input}
-        placeholder={props.placeholder}
-        value={props.value}
-        onChangeText={(num) => props.onChangeText(num)}
-        placeholderTextColor='#000000'
-        editable={props.editable !== false}
-      />
+export const Input = props => {
+  return <View>
+      <TextInput style={textStyles.input} placeholder={props.placeholder} value={props.value} onChangeText={num => props.onChangeText(num)} placeholderTextColor='#000000' editable={props.editable !== false} />
       {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
-    </View>
-  );
+    </View>;
 };
-
 const textStyles = StyleSheet.create({
   input: {
     backgroundColor: "#f7f7f7",
@@ -195,25 +214,26 @@ const textStyles = StyleSheet.create({
   }
 });
 
-
-const Button = (props) => {
-  return (
-    <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD' style={btnStyles.mainContainer}>
+const Button = props => {
+  return <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD' style={btnStyles.mainContainer}>
       <View style={[btnStyles.button, {
-        backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
-        height: props.height ? props.height : 49,
-        borderWidth: props.borderWidth ? props.borderWidth : 0,
-        borderColor: props.borderColor ? props.borderColor : "#000000"
-      }]}>
-        <Text style={[btnStyles.text, { color: props.color ? props.color : "#ffffff" }]}>{props.buttonText}</Text>
+      backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+      height: props.height ? props.height : 49,
+      borderWidth: props.borderWidth ? props.borderWidth : 0,
+      borderColor: props.borderColor ? props.borderColor : "#000000"
+    }]}>
+        <Text style={[btnStyles.text, {
+        color: props.color ? props.color : "#ffffff"
+      }]}>{props.buttonText}</Text>
       </View>
-    </TouchableHighlight>
-  );
+    </TouchableHighlight>;
 };
 
 const btnStyles = StyleSheet.create({
   mainContainer: {
-    marginVertical: 20, width: '90%', justifyContent: "center",
+    marginVertical: 20,
+    width: '90%',
+    justifyContent: "center",
     alignSelf: "center"
   },
   button: {
@@ -227,19 +247,16 @@ const btnStyles = StyleSheet.create({
     fontSize: 15
   }
 });
-
-
 import { ActivityIndicator } from "react-native";
 
 const Loader = () => {
-  return (
-    <View style={loaderStyles.container}>
+  return <View style={loaderStyles.container}>
       <View style={loaderStyles.loaderContainer}>
         <ActivityIndicator color="#000" />
       </View>
-    </View>
-  );
+    </View>;
 };
+
 const loaderStyles = StyleSheet.create({
   container: {
     width: "100%",
--- screens/index.js
+++ screens/index.js
@@ -1,56 +1,41 @@
 import React, { useEffect } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Image,
-  Dimensions,
-  Pressable
-} from "react-native";
+import { Text, View, StyleSheet, Image, Dimensions, Pressable } from "react-native";
 import { getItem } from "../../store";
-
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-const Welcome = ({ navigation }) => {
+const Welcome = ({
+  navigation
+}) => {
   const checkAuth = async () => {
-    const token = await getItem("token")
+    const token = await getItem("token");
+
     if (token) {
-      navigation.replace('homeScreen')
+      navigation.replace('homeScreen');
     }
-  }
-
-
+  };
 
   useEffect(() => {
     checkAuth();
-  }, [])
-  return (
-    <View style={styles.container}>
-      <Image
-        style={styles.background}
-        // @ts-ignore
-        source={require("./assets/welcome.png")}
-      />
+  }, []);
+  return <View style={styles.container}>
+      <Image style={styles.background} // @ts-ignore
+    source={require("./assets/welcome.png")} />
       <Text style={styles.heading}>Welcome Back</Text>
       <Text style={styles.description}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non at sed.
       </Text>
       <View style={styles.buttonContainer}>
-        <Button buttonText="Sign Up" style={styles.button} onPress={() => navigation.navigate("login", {route: "SignUpScreen"})} />
-        <Button
-          buttonText="Login"
-          style={styles.button}
-          borderColor="#000"
-          backgroundColor="#fff"
-          textColor="#000"
-          hideShadow
-          onPress={() => navigation.navigate("login", {route: "LoginScreen"})}
-        />
+        <Button buttonText="Sign Up" style={styles.button} onPress={() => navigation.navigate("login", {
+        route: "SignUpScreen"
+      })} />
+        <Button buttonText="Login" style={styles.button} borderColor="#000" backgroundColor="#fff" textColor="#000" hideShadow onPress={() => navigation.navigate("login", {
+        route: "LoginScreen"
+      })} />
       </View>
-    </View>
-  );
+    </View>;
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -87,7 +72,6 @@ const styles = StyleSheet.create({
     resizeMode: "stretch"
   }
 });
-
 export default Welcome;
 
 const Button = params => {
@@ -101,20 +85,16 @@ const Button = params => {
   const btnText = {
     color: textColor
   };
-  return (
-    <View style={[buttonStyles.btnContainer, params.style]}>
+  return <View style={[buttonStyles.btnContainer, params.style]}>
       <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
-        <Pressable
-          style={[buttonStyles.btn, btnStyle]}
-          onPress={params.onPress}>
+        <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
           <Text style={[buttonStyles.btnText, btnText]}>
             {params.buttonText}
           </Text>
           <View style={buttonStyles.childrenContainer}>{params.children}</View>
         </Pressable>
       </View>
-    </View>
-  );
+    </View>;
 };
 
 const buttonStyles = StyleSheet.create({
@@ -140,7 +120,6 @@ const buttonStyles = StyleSheet.create({
     borderRadius: 10,
     justifyContent: "center",
     alignItems: "center",
-
     flexDirection: "row"
   },
   btnText: {
