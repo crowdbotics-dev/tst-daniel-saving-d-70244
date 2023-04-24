423871058c827d7b0eaa857d18ee097179d5053f
Daniel Sousa <daniel.s@crowdbotics.com>
2023-04-24 15:00:08 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "tst-daniel-saving-d-70244",
   "url": "https://tst-daniel-saving-d-70244.botics.co",
   "api": "https://tst-daniel-saving-d-70244.botics.co/api/v1",
-  "initialRoute": "Untitled1"
+  "initialRoute": "bank-account-details"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
