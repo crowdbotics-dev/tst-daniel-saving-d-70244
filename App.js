787d8284972729f94dfbc144ad58ef8f6bc3790c
Daniel <daniel.s@crowdbotics.com>
2023-05-11 22:37:59 +0100

    remove modules and screens

--- App.js
+++ App.js
@@ -1,69 +1,86 @@
-import React, { useContext } from "react";
-import { Provider } from "react-redux";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { configureStore, createReducer, combineReducers } from "@reduxjs/toolkit";
-import { screens } from "@screens";
-import { modules, reducers, hooks, initialRoute } from "@modules";
-import { connectors } from "@store";
-const Stack = createStackNavigator();
-import { GlobalOptionsContext, OptionsContext, getOptions } from "@options";
+import React, { useContext } from "react"
+import { Provider } from "react-redux"
+import "react-native-gesture-handler"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import {
+  configureStore,
+  createReducer,
+  combineReducers
+} from "@reduxjs/toolkit"
+
+import { screens } from "@screens"
+import { modules, reducers, hooks, initialRoute } from "@modules"
+import { connectors } from "@store"
+
+const Stack = createStackNavigator()
+
+import { GlobalOptionsContext, OptionsContext, getOptions } from "@options"
 
 const getNavigation = (modules, screens, initialRoute) => {
   const Navigation = () => {
     const routes = modules.concat(screens).map(mod => {
-      const pakage = mod.package;
-      const name = mod.value.title;
-      const Navigator = mod.value.navigator;
-
+      const pakage = mod.package
+      const name = mod.value.title
+      const Navigator = mod.value.navigator
       const Component = props => {
-        return <OptionsContext.Provider value={getOptions(pakage)}>
+        return (
+          <OptionsContext.Provider value={getOptions(pakage)}>
             <Navigator {...props} />
-          </OptionsContext.Provider>;
-      };
+          </OptionsContext.Provider>
+        )
+      }
+      return <Stack.Screen key={name} name={name} component={Component} />
+    })
 
-      return <Stack.Screen key={name} name={name} component={Component} />;
-    });
-    const screenOptions = {
-      headerShown: true
-    };
-    return <NavigationContainer>
-        <Stack.Navigator initialRouteName={initialRoute} screenOptions={screenOptions}>
+    const screenOptions = { headerShown: true }
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={initialRoute}
+          screenOptions={screenOptions}
+        >
           {routes}
         </Stack.Navigator>
-      </NavigationContainer>;
-  };
-
-  return Navigation;
-};
+      </NavigationContainer>
+    )
+  }
+  return Navigation
+}
 
 const getStore = globalState => {
   const appReducer = createReducer(globalState, _ => {
-    return globalState;
-  });
+    return globalState
+  })
+
   const reducer = combineReducers({
     app: appReducer,
     ...reducers,
     ...connectors
-  });
+  })
+
   return configureStore({
     reducer: reducer,
     middleware: getDefaultMiddleware => getDefaultMiddleware()
-  });
-};
+  })
+}
 
 const App = () => {
-  const global = useContext(GlobalOptionsContext);
-  const Navigation = getNavigation(modules, screens, initialRoute);
-  const store = getStore(global);
-  let effects = {};
-  hooks.map(hook => {
-    effects[hook.name] = hook.value();
-  });
-  return <Provider store={store}>
-      <Navigation />
-    </Provider>;
-};
+  const global = useContext(GlobalOptionsContext)
+  const Navigation = getNavigation(modules, screens, initialRoute)
+  const store = getStore(global)
 
-export default App;
+  let effects = {}
+  hooks.map(hook => {
+    effects[hook.name] = hook.value()
+  })
+
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  )
+}
+
+export default App
